3fbbb563a54d6fcfba6e2b6aed65220cf7b24c67
lincontroy <lincolnmunene37@gmail.com>
2024-10-22 16:18:19 +0300

    Fixed viewport issue

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -1,5 +1,8 @@
 <!-- Meta Tag -->
 @yield('meta')
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <!-- Title Tag  -->
 <title>A minite jesus devotional</title>
 <!-- Favicon -->
